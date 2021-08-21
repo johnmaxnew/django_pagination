25fd04327a3fa6499dbc50d9dd750c1c5f42bffd
johnmaxnew <forevermeticulous@gmail.com>
2021-08-21 23:02:48 +0300

    added pagination

--- products/views.py
+++ products/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def product_list(request):
     productlist = Product.objects.all().order_by('-created')
+
+    paginator = Paginator(productlist, 3) # Show 25 contacts per page
+    page = request.GET.get('page')
+    productlist = paginator.get_page(page)
 
     return render(request, "product-list.html", {'productlist': productlist})
